7f444fc8fcdfc6a40519cd5a9d2d83120e2fa4d1
NickRowlandson <nicholasrowlandson@gmail.com>
2016-07-31 14:13:19 -0400

    Added artist model class to project. Closes issue #1

---
+++ COMP2007-Lesson10/Models/Artist.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace COMP2007_Lesson10.Models
+{
+    public class Artist
+    {
+        public int ArtistId { get; set; }
+        public string Name { get; set; }
+    }
+}
